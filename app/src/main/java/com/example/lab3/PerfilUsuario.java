ebefc741a690cb4769dfd4367b797be2e52d4612
carftorr <73551328+carftorr@users.noreply.github.com>
2021-01-13 16:31:59 -0500

    .

---
+++ app/src/main/java/com/example/lab3/PerfilUsuario.java
@@ -0,0 +1,62 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PerfilUsuario extends AppCompatActivity {
+
+    TextView txt_id, txt_name, txt_email, txt_phone;
+    ImageView imv_photo;
+    Button btn_logout;
+    Button btn_registros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_usuario);
+
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>) intent.getSerializableExtra("info_user");
+
+
+        txt_id = findViewById(R.id.txt_userId);
+        txt_name = findViewById(R.id.txt_nombre);
+        txt_email = findViewById(R.id.txt_correo);
+        txt_phone = findViewById(R.id.txt_phone);
+        imv_photo = findViewById(R.id.imv_foto);
+
+        txt_id.setText(info_user.get("user_id"));
+        txt_name.setText(info_user.get("user_name"));
+        txt_email.setText(info_user.get("user_email"));
+        txt_phone.setText(info_user.get("user_phone"));
+        String photo = info_user.get("user_photo");
+        Picasso.with(getApplicationContext()).load(photo).into(imv_photo);
+    }
+
+    public void cerrarSesion(View view) {
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", "cerrarSesion");
+        startActivity(intent);
+    }
+
+    public void irRegistros(View view){
+        Intent intent = new Intent(this, registros.class);
+        startActivity(intent);
+    }
+}
